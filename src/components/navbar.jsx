f6437d72683fcd7a84082cf32a6ff08a3d7e3289
salimzeeshan <zeeshasalim541@gmail.com>
2022-10-01 13:56:22 +0530

    day-2

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,8 @@
-import { Box, Image, Text } from "@chakra-ui/react"
+import { Box, Image, Divider, Center } from "@chakra-ui/react"
 import {Link} from "react-router-dom"
 
 export default function Navbar() {
-    return <Box fontSize={14.5} className="navbar" position={"fixed"} w="100%" display={"flex"} px={15} pr={25} alignItems={"center"} justifyContent="space-between" h={65}>
+    return <Box fontSize={14.5} className="navbar" w="100%" display={"flex"} px={15} pr={25} alignItems={"center"} justifyContent="space-between" h={65} backgroundColor={"white"} zIndex={3}>
         <Box display={"flex"} alignItems={"center"} w={"max-content"}>
             <Link to={"/"}>
                 <Image mr={7} w={158} src={"https://prod-backend-company-uploads-transcend-io.s3.amazonaws.com/57b21f6a-c3dd-4f0f-8004-f6dd369fabe2/50e3a960-4b80-46f3-b146-feddaa621ce0"} />
@@ -24,8 +24,11 @@ export default function Navbar() {
             <Box ml={25}>
                 <Link to={"/what-we-do"}>What We Do</Link>
             </Box>
+            <Center ml={25}>
+                <Divider orientation='vertical' h='35px' />
+            </Center>
             <Box ml={25}>
-                <Link to={"/sign-up"}>Sign Up</Link>
+                <Link to={"/signup"}>Sign Up</Link>
             </Box>
             <Box ml={25}>
                 <Link to={"/login"}>Login</Link>
---
+++ src/components/sliderData.jsx
@@ -0,0 +1,34 @@
+const sliderData = [
+    {
+        url: "https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/f_auto/v1664295391/tx8bsjtd4rtk3rwx9uiw.jpg",
+        title: "Coming soon! Buxus Eva",
+        desc: "An E-bike with a Motorcycle spirit"
+    },
+    {
+        url: "https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/f_auto/v1664294818/cs88q6rbzlobos2prwyf.jpg",
+        title: "Just Launched! GREAT JOY T2.9 35mm 1.8x Anamorphic Lens",
+        desc: "New Budget Anamorphic King, 1.8x Anamorphic Lens Set with Stunning Performance"
+    },
+    {
+        url: "https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/f_auto/v1664298780/rik4figygisjo2nzoqq3.jpg",
+        title: "Just Launched! IMMOTOR BAY Series Portable Power Station",
+        desc: "542.9Wh & 1023Wh|PD 100W|Lightweight|Solar Panel Charging| Wireless Charging/AC & DC| Smart App"
+    },
+    {
+        url: "https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/f_auto/v1664314845/ps3luk3boc8wvnxfatjq.jpg",
+        title: "Just Launched! The World's First Portable Ice Cream Maker",
+        desc: "Wireless & Portable | Cooling System | Handy Operation | 2-In-1 Freezer Bowl | Easy to Clean"
+    },
+    {
+        url: "https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/f_auto/v1664384014/m88aejmcelki2yox9c1s.png",
+        title: "Just Launched! Nutr",
+        desc: "Plant-Based Milk Maker"
+    },
+    {
+        url: "https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/f_auto/v1664295495/wxv8xstvbju5gkjjplbs.jpg",
+        title: "Coming Soon! Rymic E- Bike",
+        desc: "Designed Specifically For Commuters"
+    }
+]
+
+export default sliderData;
